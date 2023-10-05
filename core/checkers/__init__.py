204cc29de0a045cbf12f26153d5595ad6c89cafa
Mislav Balunovic <bmislav@ethz.ch>
2023-10-05 15:40:28 +0200

    add toxicity checker

--- core/checkers/__init__.py
+++ core/checkers/__init__.py
@@ -1,6 +1,7 @@
 from core.checkers.base import *
 from core.checkers.match import *
 from core.checkers.pii import *
+from core.checkers.toxicity import *
 
 def get_checker(checker_name: str) -> BaseChecker:
     return CheckerRegistryHolder.get_checker_registry().get(checker_name)
