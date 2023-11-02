672c11e25fafb86c7ef80b47fa26c3a2db0a4d0b
hyukhymenko <hyukhymenko@student.ethz.ch>
2023-11-02 02:26:55 +0100

    fix toxicity checker HF model

--- lve-tools/lve_tools/lve/checkers/toxicity.py
+++ lve-tools/lve_tools/lve/checkers/toxicity.py
@@ -3,7 +3,7 @@ from lve.checkers.base import BaseChecker
 class ToxicityChecker(BaseChecker):
     """Checker which judges safety based on whether the response is toxic."""
 
-    def __init__(self, toxicity_threshold: float, model: str = "valurank/distilroberta-bias", prompt_contains_responses=False, get_variables=False, get_response=True):
+    def __init__(self, toxicity_threshold: float, model: str = "s-nlp/roberta_toxicity_classifier", prompt_contains_responses=False, get_variables=False, get_response=True):
         from transformers import pipeline
         self.prompt_contains_responses = prompt_contains_responses
         self.get_variables = get_variables
